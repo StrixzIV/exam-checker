9c1b4e6bf74d27c1b6f6bdc5975f477a84fe990f
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2022-09-05 16:23:39 +0700

    detect answer mark

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ import numpy as np
 
 image = cv2.imread('./assets/neet-omr-sheet.webp')
 
-img = cv2.resize(image, (700, 700))
+roi = image[120:1315, 560:700]
+grey = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+inp = cv2.GaussianBlur(grey, ksize = (15, 15), sigmaX = 15)
 
-cv2.imshow('image', img)
+(thres, res) = cv2.threshold(inp, 110, 255, cv2.THRESH_BINARY)
+
+res = cv2.morphologyEx(res, cv2.MORPH_CLOSE, np.ones((3, 3), dtype = np.uint8), iterations = 2)
+res = cv2.dilate(res, kernel = (3, 3))
+
+(contours, hierarchy) = cv2.findContours(res, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+drawed_contours = cv2.drawContours(roi, contours[1:], -1, (0, 255, 0), 2)
+
+print(f'Found: {len(contours[1:])} of 35')
+
+cv2.imshow('image', image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
