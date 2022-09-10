a8f0154889cab01b5d056ff6c425eb8b76db1ad7
Jirayu Kaewsing <strixz.self@gmail.com>
2022-09-10 12:18:38 +0700

    Read Student ID and first 5 answers from multiple files

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import numpy as np
 
 from utils import id_block_read, read_answer, find_paper
 
-image = cv2.imread('./assets/ans_sheet1.jpg')
+imlist = ['./assets/ans_sheet1.jpg', './assets/ans_sheet2.jpg']
 
-answer_sheet = cv2.resize(find_paper(image), (827, 1669))
-student_id = id_block_read(answer_sheet)
+for img in imlist:
 
-print(student_id)
+    image = cv2.imread(img)
 
-cv2.imshow('image', answer_sheet)
+    answer_sheet = cv2.resize(find_paper(image), (827, 1669))
+    student_id = id_block_read(answer_sheet)
+
+    print(f'ID: {student_id}')
+    print(f'Answer: \n{read_answer(answer_sheet[690:845, 105:190], 5, debug = False)}')
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
