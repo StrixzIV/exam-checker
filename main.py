23b0653f37418fb758022ab9a93bb9ddac4f6694
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2022-09-13 09:35:45 +0700

    Extract read data as list of dictionaries

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os
 import cv2
 
+from tqdm import tqdm
+from rich import print
 from utils import id_block_read, read_answer, find_paper, ans_block_read
 
 imlist = ['./assets/images/' + path for path in os.listdir('./assets/images')]
 
-print('*' * 100)
+correct_ans = []
+datasets = []
 
 for img in imlist:
 
@@ -14,11 +17,16 @@ for img in imlist:
     answer_sheet = cv2.resize(find_paper(image), (827, 1669))
     student_id = id_block_read(answer_sheet, debug = False)
     answers = ans_block_read(answer_sheet, 5)
+    
+    if student_id == 0:
+        correct_ans = answers
+    
+    data = {'id': student_id, 'answers': answers}
+    datasets.append(data.copy())
 
-    print(f'ID: {student_id}, {img}')
-    print(answers, len(answers))
+datasets = sorted(datasets, key = lambda data: data['id'])
 
-    print('*' * 100)
-
+print(f'Correct answers(ID = 0): {correct_ans}')
+print(datasets)
 # cv2.waitKey(0)
 cv2.destroyAllWindows()
