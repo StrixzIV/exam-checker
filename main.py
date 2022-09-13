1448862a58ca0d8400d5df8643dba0128d00518a
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2022-09-13 20:02:50 +0700

    Process and export the readed data

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import cv2
 
+import numpy as np
+import pandas as pd
+
 from tqdm import tqdm
 from rich import print
 from image_utils import id_block_read, find_paper, ans_block_read
@@ -32,3 +35,20 @@ print(f'Correct answers(ID = 0): {correct_ans}')
 print(datasets)
 # cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+for (idx, data) in enumerate(datasets):
+    
+    datasets[idx]['answers_check'] = []
+    
+    for (base, student) in zip(correct_ans, data['answers']):
+        datasets[idx]['answers_check'].append(base == student)
+        
+for data in datasets:
+    (_, count) = np.unique(data['answers_check'], return_counts = True)
+    data['correct'] = count[1]
+    data['incorrect'] = count[0]
+    
+df = pd.DataFrame(datasets)
+df['pass'] = ["Pass" if d >= 6 else "Not pass" for d in df['correct']]
+
+df.to_excel('out.xlsx', index = False)
